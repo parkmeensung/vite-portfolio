07b2617fba213ce4681a4aa821abee21191c2b83
parkmeensung <valstjdv3@gmail.com>
2024-04-17 14:17:53 +0900

    02. intro 페이지완료

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,3 +1,21 @@
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
 export function intro(){
-    
+    gsap.registerPlugin(ScrollTrigger)
+    const frameCount = 20
+    let offsetValue = 229
+
+    gsap.to('.img',{
+        backgroundPosition:(-offsetValue*frameCount)+'px',
+        ease:'steps('+frameCount+')',
+        scrollTrigger:{
+            trigger:'#intro',
+            start: 'top top',
+            end:'+='+(frameCount*offsetValue),
+            pin:true,
+            scrub:true
+        }
+
+    })
 }
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,13 @@
 export function menu(){
-    
+    const headerToggle = document.getElementById('headerToggle')
+    const headerNav = document.querySelector('.header_nav')
+
+    if(headerToggle){
+        headerToggle.addEventListener('click',function(){
+            headerNav.classList.toggle('show')
+            headerToggle.getAttribute('aria-expanded')=='true'
+            ? headerToggle.setAttribute('aria-expanded', 'false')
+            : headerToggle.setAttribute('aria-expanded', 'true')
+        })
+    }
 }
